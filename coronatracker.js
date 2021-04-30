195d87b3ccf016825ab1900784bfc93b92d37f6f
Sahil Agarwal <agarwalsahil0210@gmail.com>
2021-04-30 11:49:10 +0530

    added stats table for coronatracker

--- coronatracker.js
+++ coronatracker.js
@@ -1,24 +1,30 @@
-//  Cases tracker
-fetch('https://api.covid19api.com/summary').then((apidata) => {
-    return apidata.json();
-}).then((realdata) => {
-    const country = realdata.Countries[76];
-    document.getElementById('1').innerHTML = country.Country;
-    document.getElementById('2').innerHTML = `Confirmed Case ${country.TotalConfirmed}`;
-    document.getElementById('3').innerHTML = `Death ${country.TotalDeaths}`;
-    document.getElementById('4').innerHTML = `Recovered ${country.TotalRecovered}`;
-}).catch((error) => {
-    console.log(`Error: ${error}`);
-});
+// //  Cases tracker
+// fetch("https://api.covid19india.org/data.json")
+//   .then((apidata) => {
+//     return apidata.json();
+//   })
+//   .then((realdata) => {
+//     const country = realdata['statewise'];
+//     console.log(country);
+//     document.getElementById("1").innerHTML = country[0]['state'];
+//     document.getElementById(
+//       "2"
+//     ).innerHTML = `Confirmed Case ${country.TotalConfirmed}`;
+//     document.getElementById("3").innerHTML = `Death ${country.TotalDeaths}`;
+//     document.getElementById(
+//       "4"
+//     ).innerHTML = `Recovered ${country.TotalRecovered}`;
+//     cell[0]
+//   })
+//   .catch((error) => {
+//     console.log(`Error: ${error}`);
+//   });
 
-
-
-// Geolocation
-if (navigator.geolocation)
-    navigator.geolocation.getCurrentPosition(function (position) {
-        console.log(position);
-        console.log(position.coords.latitude);
-        console.log(position.coords.longitude);
-    });
-else
-    console.log("Invaild");
+// // Geolocation
+// if (navigator.geolocation)
+//   navigator.geolocation.getCurrentPosition(function (position) {
+//     console.log(position);
+//     console.log(position.coords.latitude);
+//     console.log(position.coords.longitude);
+//   });
+// else console.log("Invaild");
